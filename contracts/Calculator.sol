9c414848b3ef5f9966be1c60c26f18e8843cd189
Dragon-S20 <promoetherjs.dragons20@gmail.com>
2020-10-29 03:35:45 -0400

    tools dev correction

---
+++ contracts/Calculator.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+import "./Adder.sol";
+import "./Subber.sol";
+import "./Multiplier.sol";
+import "./Divisor.sol";
+
+contract Calculator {
+    Adder private _adderContract;
+    Suber private _suberContract;
+    Multiplier private _multiplierContract;
+    Divisor private _divisorContract;
+
+    constructor(
+        address _adderContract,
+        address _suberContract,
+        address _multiplierContract,
+        address _divisorContract
+    ) public {
+        _adderContract = address(Adder(_adderContract));
+        _suberContract = address(Suber(_suberContract));
+        _multiplierContract = address(Multiplier(_multiplierContract));
+        _divisorContract = address(Divisor(_divisorContract));
+    }
+
+    function add(uint256 nb1, uint256 nb2) public view returns (uint256) {
+        return _adderContract.add(nb1, nb2);
+    }
+
+    function sub(uint256 nb1, uint256 nb2) public view returns (uint256) {
+        return _suberContract.sub(nb1, nb2);
+    }
+
+    function mul(uint256 nb1, uint256 nb2) public view returns (uint256) {
+        return _multiplierContract.mul(nb1, nb2);
+    }
+
+    function div(uint256 nb1, uint256 nb2) public view returns (uint256) {
+        return _divisorContract.div(nb1, nb2);
+    }
+}
